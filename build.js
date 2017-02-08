f28654395ba516f15f4dfa353a7d35a8359d4ae6
Martin Abelson Sahlen <martin@unacast.com>
2017-02-08 18:02:38 +0100

    Remove unneded stuff

--- build.js
+++ build.js
@@ -2,12 +2,7 @@ var path = require("path");
 var fs = require("fs-extra");
 var fable = require("fable-compiler");
 
-// We will use `npm` as the build directory as it will contain
-// the contents of the package to be published in npm. However,
-// you can give it another name if you prefer.
 var BUILD_DIR = "npm"
-var PKG_JSON = "package.json"
-var README = "README.md"
 var RELEASE_NOTES = "RELEASE_NOTES.md"
 var PROJ_FILE = "src/Fable.Helpers.Sample.fsproj"
 
@@ -17,15 +12,7 @@ var PROJ_FILE = "src/Fable.Helpers.Sample.fsproj"
 var fableconfig = {
     projFile: PROJ_FILE,
     outDir: BUILD_DIR,
-    dll: true,
-    sourceMaps: true
-};
-
-var fableconfigUMD = {
-    projFile: PROJ_FILE,
-    outDir: path.join(BUILD_DIR,"umd"),
-    module: "umd",
-    sourceMaps: true
+    module: 'commonjs',
 };
 
 var targets = {
@@ -37,28 +24,9 @@ var targets = {
             // When this is complete, compile the F# project using Fable
             .then(_ => fable.compile(fableconfig))
             // According to the convention for Fable packages, compile the project
-            // again with UMD modules and put the files in a `umd` subdirectory
-            .then(_ => fable.compile(fableconfigUMD))
             // Run a simple test to make sure the build is correct
-            .then(_ => fable.compile("test"))
-            // Copy `package.json` and `README.md` into the build directory
-            .then(_ => fable.promisify(fs.copy, PKG_JSON, path.join(BUILD_DIR, PKG_JSON)))
-            .then(_ => fable.promisify(fs.copy, README, path.join(BUILD_DIR, README)))
             // Now let's take the latest version from the release notes and update package.json
-            .then(_ => fable.promisify(fs.readFile, RELEASE_NOTES))
-            .then(releaseNotes => {
-                // Capture the version number on top of release notes
-                var version = /\d[^\s]*/.exec(releaseNotes)[0];
-                // Now run "npm version" command to update the version
-                // in the `package.json` copy within the build directory
-                return fable.runCommand(BUILD_DIR, "npm version " + version);
-            });
     },
-    publish() {
-        // Because all targets return promises, chaining them becomes trivial
-        return this.all()
-            .then(_ => fable.runCommand(BUILD_DIR, "npm publish"))
-    }
 }
 
 // As with FAKE scripts, run a default target if no one is specified
--- src/MyLib.fsx
+++ src/MyLib.fsx
@@ -6,8 +6,5 @@ open System
 open Fable.Core
 open Fable.Import
 
-/// Use example
-/// [ ("A", 234.45); ("B", 23458.0214) ]
-/// |> printPairsPadded 3 12
 let helloWorld (request: express.Request) (response: express.Response) =
     response.send "Hello, World!"
